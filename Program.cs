7bbadbc11731ddc39e0d633a314ba853b2e0ea3a
Home <none@gmail.com>
2023-07-22 09:13:10 +0300

    initial

--- Program.cs
+++ Program.cs
@@ -5,13 +5,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -29,6 +33,7 @@ else
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -38,6 +43,32 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
+app.MigrateDatabase();
+
 app.Run();
+
+public static class MigrationManager
+{
+    public static WebApplication MigrateDatabase(this WebApplication webApp)
+    {
+        using (var scope = webApp.Services.CreateScope())
+        {
+            using (var appContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>())
+            {
+                try
+                {
+                    appContext.Database.Migrate();
+
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+        return webApp;
+    }
+}
